44283e8b64af5cd844ad84c76ee0901fc3c908d3
raj68518 <raj68518@gmail.com>
2020-08-19 18:03:23 +0530

    created dynemic work list data

--- js/jsonData.js
+++ js/jsonData.js
@@ -1,54 +1,82 @@
-const myDb ={
+const myDb = {
     workList :[
+         {
+             id: 0,
+             siteTitle: "Expense Tracking App",
+             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
+             siteThumbnails: '/img/project-img/shoestore.jpg'
+         },
         {
+            id: 1,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 2,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 3,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 4,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 5,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 6,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 7,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 8,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
         {
+            id: 9,
             siteTitle: "Expense Tracking App",
             webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
             siteThumbnails: 'api.google.com'
         },
+    ],
+    skillList:[
         {
-            siteTitle: "Expense Tracking App",
-            webUrl: "https://adityapanther.github.io/Expense-Tracking-app",
-            siteThumbnails: 'api.google.com'
+            id:0,
+            skill_title: "Html"
+        },
+        {
+            id: 1,
+            skill_title: "css"
+        },
+        {
+            id: 2,
+            skill_title: "javascript"
+        },
+        {
+            id: 3,
+            skill_title: "Sass"
         },
     ]
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
-// 
+var projectItemsContainer = document.getElementsByClassName('project-container')[0];
+var workData = myDb.workList
+var skillData = myDb.skillList
 
-window.addEventListener("load", (e)=>{
-    console.log("Loaded");
-    
+
+
+window.addEventListener('load', ()=>{
+    loadProjectData()
 })
+
+
+function loadProjectData(){
+    workData.map(data => {
+        addDataToProjectContainer(data)
+
+    })
+}
+
+console.log(window.location.pathname+ "img");
+
+
+function addDataToProjectContainer(data){
+
+    var dom = `<div class="project-item" id="${data.id}">
+    <a href="${data.webUrl}">
+        <div class="porject-item-img">
+            <img src="${data.siteThumbnails}"
+             alt="${data.siteTitle}" />
+        </div>
+        <div class="project-item-title">
+            <h1>${data.siteTitle}</h1>
+        </div>
+        </a>
+        </div>`;
+
+    projectItemsContainer.innerHTML += dom;
+
+}
+
