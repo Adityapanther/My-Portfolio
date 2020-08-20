91a15d92f161df1e7894360258f2fd98d3cadb85
raj68518 <raj68518@gmail.com>
2020-08-20 19:34:22 +0530

    skill data added and hard coded skill removed

--- js/jsonData.js
+++ js/jsonData.js
@@ -72,11 +72,55 @@ const myDb = {
         },
         {
             id: 2,
-            skill_title: "javascript"
+            skill_title: "Sass"
         },
         {
             id: 3,
-            skill_title: "Sass"
+            skill_title: "javascript"
         },
+        {
+            id: 4,
+            skill_title: "Node js"
+        },
+        {
+            id: 5,
+            skill_title: "Express"
+        },
+        {
+            id: 6,
+            skill_title: "React js"
+        },
+        {
+            id: 7,
+            skill_title: "Ajax"
+        },
+        {
+            id: 8,
+            skill_title: "Json"
+        },
+        {
+            id: 9,
+            skill_title: "Firebase"
+        },
+        {
+            id: 10,
+            skill_title: "MySql"
+        },
+        {
+            id: 11,
+            skill_title: "MongoDb"
+        },
+        {
+            id: 12,
+            skill_title: "kotlin"
+        },
+        {
+            id: 13,
+            skill_title: "android"
+        },
+        {
+            id: 14,
+            skill_title: "flutter"
+        }
     ]
 }
